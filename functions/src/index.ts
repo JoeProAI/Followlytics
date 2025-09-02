396f0018bd237f8d68ebe26add3c8d2f42b9a74e
Joseph Gaither <joe@joepro.ai>
2025-09-02 13:53:04 -0400

    Remove Firebase functions directory from repository - deploy separately

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-admin.initializeApp();
-
-export { pollFollowers } from './followerTracking';
-export { processUnfollows } from './grokAnalysis';
-export { sendNotifications } from './notifications';
